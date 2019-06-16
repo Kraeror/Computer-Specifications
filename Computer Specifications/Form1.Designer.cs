6d7f2be92538a30c76a195aaec86db7cd236cf19
Kraeror <49652516+Kraeror@users.noreply.github.com>
2019-06-16 14:35:46 +0300

    Computer Specifications

---
+++ Computer Specifications/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Computer_Specifications
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label5;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.serviceController1 = new System.ServiceProcess.ServiceController();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            label5.BackColor = System.Drawing.Color.Transparent;
+            label5.Font = new System.Drawing.Font("Modern No. 20", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label5.ForeColor = System.Drawing.Color.Red;
+            label5.Location = new System.Drawing.Point(6, 152);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(282, 112);
+            label5.TabIndex = 10;
+            label5.Text = "Credits";
+            label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Computer_Specifications.Properties.Resources.Button;
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(608, 437);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 112);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Computer_Specifications.Properties.Resources.Title;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(888, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Computer_Specifications.Properties.Resources.OS;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 437);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(135, 112);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Computer_Specifications.Properties.Resources.GPU;
+            this.pictureBox3.Location = new System.Drawing.Point(153, 319);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(135, 112);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Computer_Specifications.Properties.Resources.RAM_icon;
+            this.pictureBox4.Location = new System.Drawing.Point(294, 201);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(135, 112);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Computer_Specifications.Properties.Resources.CPU;
+            this.pictureBox5.Location = new System.Drawing.Point(430, 86);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(135, 112);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(178, 437);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 112);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Operating System (OS)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(301, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(400, 112);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Video Card";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(446, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(400, 112);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Random Acess Memory (RAM)";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(571, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(315, 112);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Central Processing Unit (CPU)";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Computer Specifications";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.ServiceProcess.ServiceController serviceController1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Computer Specifications/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Management.Instrumentation;
+using System.Management;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace Computer_Specifications
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetPhysicallyInstalledSystemMemory(out long TotalMemoryInKilobytes);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "Random Acess Memory (RAM)")
+            {
+                long kbs;
+                GetPhysicallyInstalledSystemMemory(out kbs).ToString();
+                var result = kbs / 1024 / 1024;
+                if (result < 1)
+                {
+                    var mbs = kbs / 1024;
+                    label3.Text = "You've got less than a gigabyte of random acess memory (" + mbs + " megabytes)!";
+                }
+                else if (result == 1)
+                {
+                    label3.Text = "You've got only ONE GB of random acess memory!";
+                }
+                else
+                {
+                    label3.Text = "You've got " + result.ToString() + " GBs of random acess memory!";
+                }
+                label6.Text = GetCPU();
+                label2.Text = GetGPU();
+                label1.Text = GetOS().Substring(0, GetOS().LastIndexOf("|C")).Trim();
+            }
+        }
+        public static string RemoveLastChars(string input, int amount = 2)
+        {
+            if (input.Length > amount)
+                input = input.Remove(input.Length - amount);
+            return input;
+        }
+        public static string GetCPU()
+        {
+            try
+            {
+                string Name = string.Empty;
+                using (ManagementObjectSearcher MOS = new ManagementObjectSearcher("SELECT * FROM Win32_Processor"))
+                {
+                    foreach (ManagementObject MO in MOS.Get()) Name += MO["Name"] + "; ";
+                }
+
+                Name = RemoveLastChars(Name);
+                return !string.IsNullOrEmpty(Name) ? Name : "N/A";
+            }
+            catch
+            {
+            }
+
+            return "N/A";
+        }
+        public static string GetGPU()
+        {
+            try
+            {
+                string Name = string.Empty;
+                using (ManagementObjectSearcher MOS =
+                    new ManagementObjectSearcher("SELECT * FROM Win32_DisplayConfiguration"))
+                {
+                    foreach (ManagementObject MO in MOS.Get()) Name += MO["Description"] + " ;";
+                }
+
+                Name = RemoveLastChars(Name);
+                return !string.IsNullOrEmpty(Name) ? Name : "N/A";
+            }
+            catch
+            {
+                return "N/A";
+            }
+        }
+        public static string GetOS()
+        {
+            try
+            {
+                string Name = string.Empty;
+                using (ManagementObjectSearcher MOS = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem"))
+                {
+                    foreach (ManagementObject MO in MOS.Get()) Name += MO["Name"] + "; ";
+                }
+
+                Name = RemoveLastChars(Name);
+                return !string.IsNullOrEmpty(Name) ? Name : "N/A";
+            }
+            catch
+            {
+            }
+
+            return "N/A";
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("AgentXzX (A.K.A. Kraeror) - The Developer!\nNinjahZ - Helped with much constructors!", "Credits");
+        }
+    }
+}
